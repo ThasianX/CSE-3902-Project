8fb23b0eabb04ac8d07c6fb96502d61f44c1b727
Kevin Li <kevinli29033@gmail.com>
2021-10-02 01:35:57 -0400

    Added reset command (#25)

---
+++ Project1/Commands/ResetCommand.cs
@@ -0,0 +1,19 @@
+﻿using Project1.Interfaces;
+
+namespace Project1.Commands
+{
+    class ResetCommand: ICommand
+    {
+        private readonly Game1 myGame;
+
+        public ResetCommand(Game1 game)
+        {
+            myGame = game;
+        }
+
+        public void Execute()
+        {
+            myGame.Reset();
+        }
+    }
+}
