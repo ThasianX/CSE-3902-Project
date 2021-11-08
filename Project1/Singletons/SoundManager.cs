48ad9aabf07c8008c11eaa09a05575b238021e33
Keenan Anderson <86087556+KeenanRAnderson@users.noreply.github.com>
2021-11-07 23:12:36 -0500

    Added SoundManager and Sounds

---
+++ Project1/Singletons/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Project1
+{
+    class SoundManager
+    {
+        private Dictionary<string, SoundEffect> loadedSounds = new Dictionary<string, SoundEffect>();
+
+        private static SoundManager instance = new SoundManager();
+
+        public static SoundManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void PlaySound(string soundName)
+        {
+            loadedSounds[soundName].Play();
+        }
+
+        public void LoadAllSounds(ContentManager content)
+        {
+            DirectoryInfo dir = new DirectoryInfo(content.RootDirectory + "/SoundEffects");
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException();
+
+            FileInfo[] files = dir.GetFiles("*.*");
+            foreach (FileInfo file in files)
+            {
+                loadedSounds.Add(file.ToString(), content.Load<SoundEffect>("SoundEffects/" + file.Name.Split('.')[0]));
+            }
+        }
+    }
+}
