9eef10093d8931faccf87eec155481625014c092
Zheyuan G <90584603+JellyFishGao@users.noreply.github.com>
2021-11-12 21:57:52 -0500

    Feature/sprint reflection (#80)
    * Create half block to prevent link get off the ground in the dungeon item room
    
    * Finished sprint reflection
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ Project1/Objects/Blocks/HalfBlock.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project1.Interfaces;
+
+namespace Project1.Objects
+{
+    public class HalfBlock : IBlock, ICollidable
+    {
+        public Vector2 Position { get; set; }
+
+        ISprite sprite;
+        public bool IsMover => false;
+        public string CollisionType => "Block";
+        public HalfBlock(Vector2 position)
+        {
+            this.Position = position;
+            sprite = SpriteFactory.Instance.CreateSprite("half_block");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch, Position);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            sprite.Update(gameTime);
+        }
+
+        public Rectangle GetRectangle()
+        {
+            return new Rectangle((int)Position.X, (int)Position.Y, 16, 8);
+        }
+    }
+}
