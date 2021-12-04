0417f9ba9d467bf143dcc99436b6def05ca0e8a5
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2021-12-03 19:18:51 -0500

    Possible feature/maze (#84)
    * just a possible feature of maze generation
    
    * add some comments on code

---
+++ Project1/Maze/MazeQuery.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Project1.Maze
+{
+    public static class MazeQuery
+    {
+        // query is like a List of vector2 where each dead end grid value is stored
+        public static IEnumerable<(int Row, int Column)> DeadEnds(this GridGraph<Direction> directionGridGraph)
+        {
+            var query = from grid in directionGridGraph
+                        where grid.NodeValue.IsDeadEnd()
+                        select (grid.Row, grid.Column);
+            return query;
+        }
+
+        public static IEnumerable<(int Row, int Column)> TJunctions(this GridGraph<Direction> directionGridGraph)
+        {
+            var query = from grid in directionGridGraph
+                        where grid.NodeValue.IsTJunction()
+                        select (grid.Row, grid.Column);
+            return query;
+        }
+
+        public static IEnumerable<(int Row, int Column)> Straights(this GridGraph<Direction> directionGridGraph)
+        {
+            var query = from grid in directionGridGraph
+                        where grid.NodeValue.IsStraight()
+                        select (grid.Row, grid.Column);
+            return query;
+        }
+
+        public static IEnumerable<(int Row, int Column)> Turns(this GridGraph<Direction> directionGridGraph)
+        {
+            var query = from grid in directionGridGraph
+                        where grid.NodeValue.IsTurn()
+                        select (grid.Row, grid.Column);
+            return query;
+        }
+
+        public static IEnumerable<(int Row, int Column)> CrossSections(this GridGraph<Direction> directionGridGraph)
+        {
+            var query = from grid in directionGridGraph
+                        where grid.NodeValue.IsCrossSection()
+                        select (grid.Row, grid.Column);
+            return query;
+        }
+    }
+}
