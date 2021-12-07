42cfc935998af938346cc6ed8c2741af1c070f86
Kevin Li <kevinli29033@gmail.com>
2021-12-06 21:57:20 -0500

    Fixed boss to triforce room

--- Project1/Levels/LevelGenerator.cs
+++ Project1/Levels/LevelGenerator.cs
@@ -68,7 +68,7 @@ namespace Project1.Levels
                 }
             }
 
-            MakeDoor(rooms[^1], Maze.Direction.East, GetRoomId(rows, cols));
+            MakeDoor(rooms[^1], Maze.Direction.East, GetRoomId(rows - 1, cols));
             LoadRoom(rows - 1, cols,
                 specialElements.Find(e => e.Attribute("id").Value == "triforce"),
                 new Dictionary<Maze.Direction, bool>()
