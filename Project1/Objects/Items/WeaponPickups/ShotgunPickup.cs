99a9de07e46ad8f24a3e2e92ef7bc813a2145253
Keenan Anderson <86087556+KeenanRAnderson@users.noreply.github.com>
2021-12-07 00:31:51 -0500

    ShotgunPickup

---
+++ Project1/Objects/Items/WeaponPickups/ShotgunPickup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Project1.Interfaces;
+
+namespace Project1.Objects
+{
+    public class ShotgunPickup : IEquippable, ICollidable
+    {
+        public static ShotgunPickup staticInstance = new ShotgunPickup(Vector2.Zero);
+        public IInventoryItem StaticInstance
+        {
+            get { return staticInstance; }
+        }
+        public string Name => "Shotgun";
+        public bool IsConsumable => false;
+        public int MaxStackCount => 1;
+        public Vector2 Position { get; set; }
+        public ISprite Sprite { get; set; }
+        public bool IsMover => false;
+        public string CollisionType => "PickUp";
+        public ShotgunPickup(Vector2 position)
+        {
+            this.Position = position;
+            Sprite = SpriteFactory.Instance.CreateSprite("shotgun_right");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Sprite.Draw(spriteBatch, Position);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Sprite.Update(gameTime);
+        }
+
+        public void Use(IPlayer player)
+        {
+                player.ShootShotGun();
+        }
+
+        public Rectangle GetRectangle()
+        {
+            Dimensions dimensions = Sprite.GetDimensions();
+            return new Rectangle((int)Position.X, (int)Position.Y, dimensions.width, dimensions.height);
+        }
+    }
+}
